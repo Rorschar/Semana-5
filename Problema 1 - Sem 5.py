825683cda47d80aabcb4042ec9ad82b025f25549
Rorschar <119158777+Rorschar@users.noreply.github.com>
2022-12-21 21:57:41 -0500

    Add files via upload

---
+++ Problema 1 - Sem 5.py
@@ -0,0 +1,5 @@
+#Una persona va invertir su dinero en un banco y quiere saber cuanto dinero ganara después de un mes si el banco paga a razón de 2% mensual.
+
+a=float(input("Ingrese la cantidad que desea invertir al banco: "))
+b=a*1.02
+print("Esta es la cantidad que ganara despues de un mes: " , b)
